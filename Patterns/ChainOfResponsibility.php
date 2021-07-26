7f85e72377589c93c19c06cbc08590c2cdbb3d4a
Boy Havelaar <boy.havelaar@radancy.com>
2021-07-26 20:18:54 +0200

    doc234135

--- Patterns/ChainOfResponsibility.php
+++ Patterns/ChainOfResponsibility.php
@@ -15,14 +15,15 @@ interface HandlerInterface
 
     /**
      * in een Laravel Middleware wordt in de handle de $request,
-     * een Closure $next en $attributes als param meegegeven, in dit voorbeeld doen we alleen $attributes
+     * een Closure $next en $request als param meegegeven,
+     * in dit voorbeeld doen we een array met wat request settings
      */
-    public function handle($attributes = null);
+    public function handle($request = null);
 
     /**
      * De volgende handler die uitgevoerd moet worden
      */
-    public function next($attributes = null);
+    public function next($request = null);
 }
 
 abstract class AbstractHandler implements HandlerInterface
@@ -42,10 +43,10 @@ abstract class AbstractHandler implements HandlerInterface
     /**
      * Roep volgende handle() aan als die klaar staat, anders zijn we door de chain heen
      */
-    public function next($attributes = null)
+    public function next($request = null)
     {
         if ($this->next) {
-            return $this->next->handle($attributes);
+            return $this->next->handle($request);
         }
     }
 }
@@ -54,46 +55,46 @@ class IPCheckHandler extends AbstractHandler
 {
     const BANNED_IPS = ['123.123.123.123'];
 
-    public function handle($attributes = null)
+    public function handle($request = null)
     {
         print __METHOD__ . 'Checking if valid IP ...' . PHP_EOL;
 
-        if (in_array($attributes['ip'], self::BANNED_IPS, true)) {
+        if (in_array($request['ip'], self::BANNED_IPS, true)) {
             throw new Exception("Invalid IP");
         }
 
-        return $this->next($attributes);
+        return $this->next($request);
     }
 }
 
 class MustBeLoggedInHandler extends AbstractHandler
 {
-    public function handle($attributes = null)
+    public function handle($request = null)
     {
         print __METHOD__ . 'Checking if user logged in ...' . PHP_EOL;
-        if (!$attributes['user_id']) {
+        if (!$request['user_id']) {
             throw new Exception("Must be logged in");
         }
 
-        return $this->next($attributes);
+        return $this->next($request);
     }
 }
 
 class MustBeBoyHandler extends AbstractHandler
 {
-    public function handle($attributes = null)
+    public function handle($request = null)
     {
         print __METHOD__ . 'Check if user is Boy ...' . PHP_EOL;
-        if (!$attributes['is_admin']) {
+        if (!$request['is_admin']) {
             throw new Exception("Must be Boy");
         }
 
-        return $this->next($attributes);
+        return $this->next($request);
     }
 }
 
 
-$attributes = [
+$request = [
     'ip' => '127.0.0.1',
     'requested_uri' => '/home',
     'user_id' => 123,
@@ -107,4 +108,4 @@ $boyCheck = new MustBeBoyHandler();
 $mustBeLoggedIn->setNext($ipCheck);
 $ipCheck->setNext($boyCheck);
 
-$mustBeLoggedIn->handle($attributes);
+$mustBeLoggedIn->handle($request);
